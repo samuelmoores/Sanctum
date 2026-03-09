d1399354882201fd58f7512bfbd4be39f22f8d1b
Isaiah Pone <isaiahpone@gmail.com>
2026-03-09 10:34:55 -0700

    A new source code for rooms on this model

---
+++ Sanctum/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace Sanctum.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public TimeOnly Time_Limit  { get; set; }
+        public int Capacity { get; set; }
+        public int Availbility { get; set; }
+    }
+}
