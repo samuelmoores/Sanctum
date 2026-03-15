32b4d058883898e69c93366901fcb65148d6e987
julia <jmatinyan17@gmail.com>
2026-03-15 03:05:31 -0700

    room booking done: map added + minor design fixes

--- Sanctum/Data/SeedData.cs
+++ Sanctum/Data/SeedData.cs
@@ -9,22 +9,24 @@ public class SeedData
         var desiredRooms = new List<Room>
         {
             // ECS
-            new Room { Building = "ECS", RoomName = "ECS-201" },
-            new Room { Building = "ECS", RoomName = "ECS-202" },
-            new Room { Building = "ECS", RoomName = "ECS-301" },
+            new Room { Building = "COB", RoomName = "COB-201" },
+            new Room { Building = "COB", RoomName = "COB-202" },
+            new Room { Building = "COB", RoomName = "COB-301" },
+            new Room { Building = "COB", RoomName = "COB-302" },
             // LIB
-            new Room { Building = "LIB", RoomName = "LIB-101" },
-            new Room { Building = "LIB", RoomName = "LIB-203" },
-            new Room { Building = "LIB", RoomName = "LIB-305" },
+            new Room { Building = "HC", RoomName = "HC-101" },
+            new Room { Building = "HC", RoomName = "HC-203" },
+            new Room { Building = "HC", RoomName = "HC-305" },
+            new Room { Building = "HC", RoomName = "HC-306" },
             // USU
-            new Room { Building = "USU", RoomName = "USU-110" },
-            new Room { Building = "USU", RoomName = "USU-210" },
+            new Room { Building = "SSSC", RoomName = "SSSC-110" },
+            new Room { Building = "SSSC", RoomName = "SSSC-210" },
             // VEC
             new Room { Building = "VEC", RoomName = "VEC-17" },
             new Room { Building = "VEC", RoomName = "VEC-18" },
             new Room { Building = "VEC", RoomName = "VEC-19" },
             new Room { Building = "VEC", RoomName = "VEC-20" }
-
+            
             };
 
         foreach (var room in desiredRooms)
