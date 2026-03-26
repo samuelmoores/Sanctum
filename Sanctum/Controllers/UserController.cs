8e767573d9e3db3d66d2c75b9bdac4246b42127c
samuelmoores <97479626+samuelmoores@users.noreply.github.com>
2026-03-26 09:09:47 -0700

    UserController with UpdateEmail() added

---
+++ Sanctum/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sanctum.Data;
+using System.Security.Claims;
+
+namespace Sanctum.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public UserController(AppDbContext _context)
+        {
+            this._db = _context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmail(IFormCollection collection)
+        {
+            var id = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var newEmail = collection["Email"].ToString().Trim();
+            var user = _db.Users.Find(id);
+
+            if (user == null)
+            {
+                throw new KeyNotFoundException($"User with ID {id} not found.");
+            }
+
+            if (_db.Users.Any(u => u.Username == newEmail && u.Id != id))
+                throw new InvalidOperationException("Email is already in use by another user.");
+
+            user.Username = newEmail;
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(HomeController.Index));
+        }
+    }
+}
